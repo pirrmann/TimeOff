c9d009851ad78a9533d0cdcbb409c138e3700f96
Pierre Irrmann <pierre.irrmann@gmail.com>
2017-12-11 14:22:04 +0100

    Fix the tests

--- tests/TestScenarios/EmployeeManagement.fs
+++ tests/TestScenarios/EmployeeManagement.fs
@@ -1,40 +1,42 @@
 module TestsScenarios.EmployeeManagement
 
-  open Shared.Types
+open System
+open Shared.Types
+open Suave.Logging
 
-  type Given =
-    | ``An employee already exists`` of Employee
+type Given =
+  | ``An employee already exists`` of Employee
 
-  type When =
-    | ``Create an employee`` of Employee
+type When =
+  | ``Create an employee`` of Employee
 
-  type Then =
-    | ``The employee exists`` of Employee
-    | ``There was an error``
+type Then =
+  | ``The employee exists`` of Employee
+  | ``There was an error``
 
-  let scenarios = [
-    {
-      Name = "Create first employee"
-      Given = []
-      When = ``Create an employee`` { UserName = "jdoe"; FirstName = "John"; LastName = "Doe" }
-      Then = [``The employee exists`` { UserName = "jdoe"; FirstName = "John"; LastName = "Doe" }]
-    }
+let scenarios = [
+  {
+    Name = "Create first employee"
+    Given = []
+    When = ``Create an employee`` { UserName = "jdoe"; FirstName = "John"; LastName = "Doe"; StartDate = DateTime(2017, 12, 1); MonthlyVacationRate = 2. }
+    Then = [``The employee exists`` { UserName = "jdoe"; FirstName = "John"; LastName = "Doe"; StartDate = DateTime(2017, 12, 1); MonthlyVacationRate = 2. }]
+  }
 
-    {
-      Name = "Create second employee"
-      Given = [``An employee already exists`` { UserName = "jdoe"; FirstName = "John"; LastName = "Doe" }]
-      When = ``Create an employee`` { UserName = "barctor"; FirstName = "Bob"; LastName = "Arctor" }
-      Then = [``The employee exists`` { UserName = "barctor"; FirstName = "Bob"; LastName = "Arctor" }]
-    }
+  {
+    Name = "Create second employee"
+    Given = [``An employee already exists`` { UserName = "jdoe"; FirstName = "John"; LastName = "Doe"; StartDate = DateTime(2017, 12, 1); MonthlyVacationRate = 2. }]
+    When = ``Create an employee`` { UserName = "barctor"; FirstName = "Bob"; LastName = "Arctor"; StartDate = DateTime(2017, 12, 1); MonthlyVacationRate = 2. }
+    Then = [``The employee exists`` { UserName = "barctor"; FirstName = "Bob"; LastName = "Arctor"; StartDate = DateTime(2017, 12, 1); MonthlyVacationRate = 2. }]
+  }
 
-    {
-      Name = "Create employee with already used UserName"
-      Given = [``An employee already exists`` { UserName = "jdoe"; FirstName = "John"; LastName = "Doe" }]
-      When = ``Create an employee`` { UserName = "jdoe"; FirstName = "Jane"; LastName = "Doe" }
-      Then =
-        [
-          ``There was an error``
-          ``The employee exists`` { UserName = "jdoe"; FirstName = "John"; LastName = "Doe" }
-        ]
-    }
-  ]
+  {
+    Name = "Create employee with already used UserName"
+    Given = [``An employee already exists`` { UserName = "jdoe"; FirstName = "John"; LastName = "Doe"; StartDate = DateTime(2017, 12, 1); MonthlyVacationRate = 2. }]
+    When = ``Create an employee`` { UserName = "jdoe"; FirstName = "Jane"; LastName = "Doe"; StartDate = DateTime(2017, 12, 1); MonthlyVacationRate = 2. }
+    Then =
+      [
+        ``There was an error``
+        ``The employee exists`` { UserName = "jdoe"; FirstName = "John"; LastName = "Doe"; StartDate = DateTime(2017, 12, 1); MonthlyVacationRate = 2. }
+      ]
+  }
+]
